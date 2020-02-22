b1400ade47a429e87968bcc43e97cdf5b05778b5
Aleksandr <rabbit_1399@icloud.com>
2020-02-23 02:08:23 +0300

    complete getting all users by role

--- web_backend/api/authentication.py
+++ web_backend/api/authentication.py
@@ -3,23 +3,26 @@ from flask import request, jsonify
 
 from web_backend import db, auth
 from web_backend.api import bp
-from web_backend.binder.methods_user import verify_password
+from web_backend.binder.methods_user import verify_password, users_by_role
 from web_backend.database.models import User, Role
 from flask_user import roles_required
 
 
 @bp.route("/test", methods=["GET"])
 def test():
+
     # user = User(
-    #     username="sys360",
-    #     email="sys360@sys360.com",
+    #     username="mana",
+    #     email="sys36s@sys0.com",
     #     password_hash=(bcrypt.hashpw(b"sys360", bcrypt.gensalt(15))).decode('UTF-8'),
-    #     first_name="sys360",
-    #     last_name="sys360",
-    #     role=2
+    #     first_name="sys0",
+    #     last_name="sys0",
+    #     role=1
     # )
     # db.session.add(user)
     # db.session.commit()
+    users = users_by_role("Manager")
+    print(users)
     return f"<h1>Hello World! </h1>"
 
 
@@ -30,8 +33,11 @@ def login():
         return jsonify({"status": "200", "role": "Admin"})
 
 
-@bp.route("/users?roleId=")
+@bp.route("/users", methods=["GET"])
 def get_users_by_role():
-    role_id = request.args.get("roleId", type=int)
-
+    role_id = request.args["roleId"]
+    users = users_by_role(role_id)
+    if users:
+        return jsonify(users)
+    return jsonify(status=400, message="No users with this roles.")
 
--- web_backend/binder/methods_user.py
+++ web_backend/binder/methods_user.py
@@ -3,7 +3,7 @@ import random
 import bcrypt
 
 from web_backend import auth
-from web_backend.database.models import User
+from web_backend.database.models import User, Role
 
 
 @auth.hash_password
@@ -23,6 +23,21 @@ def verify_password(uname: str, password: str):
     return False
 
 
-def users_by_role():
+def users_by_role(role):
     all_users = []
-    users = User
+    role_id = Role.query.filter_by(name=role).first()
+    if role_id is None:
+        return all_users
+    users = User.query.filter_by(role=role_id.id).all()
+    if users is None:
+        return all_users
+    for user in users:
+        all_users.append({
+            "userId": user.id,
+            "userName": user.username,
+            "roleId": role,
+            "firstName": user.first_name,
+            "lastName": user.last_name,
+            "userPhone": user.phone
+        })
+    return all_users
