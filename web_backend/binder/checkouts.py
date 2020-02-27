c3568310a049a382ee7c30160b239b6070413272
Aleksandr <rabbit_1399@icloud.com>
2020-02-28 01:10:53 +0300

    FIX MOTHERFUCKERS

--- web_backend/binder/checkouts.py
+++ web_backend/binder/checkouts.py
@@ -59,7 +59,7 @@ def checkouts_post(data):
     for field in ["shop_id", "start", "end", "worker", "type"]:
         if field in data:
             if field == "start" or field == "end":
-                result[field] = datetime.strptime(str(data[field]), "%d-%m-%Y %H:%M:S")\
+                result[field] = datetime.strptime(data[field], "%d-%m-%Y %H:%M:%S")\
                     .replace(tzinfo=timezone.utc).timestamp()
                 continue
             result[field] = data[field]
