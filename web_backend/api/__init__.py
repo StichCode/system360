fda06cc537f36ac60adde4771b141bdc63a85d88
Aleksandr <rabbit_1399@icloud.com>
2020-03-25 01:58:34 +0300

    fix init

--- web_backend/api/__init__.py
+++ web_backend/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint("api", __name__)
 
-from web_backend.api import authentication, tokens, map_of_shop, main_routes
+from web_backend.api import authentication, tokens, map_of_shop, main_data
